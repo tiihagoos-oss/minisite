17bf560f3a38ef4d2b36d14e88bd6ffaad57f30b
tiihagoos-oss <tiihagoos@gmail.com>
2026-01-19 15:15:32 -0300

    Update log.js

--- api/log.js
+++ api/log.js
@@ -1,1 +1,8 @@
+export default function handler(req, res) {
+  const country =
+    req.headers["x-vercel-ip-country"] || "Desconhecido";
 
+  console.log("ACESSO | País:", country);
+
+  res.status(200).end();
+}
